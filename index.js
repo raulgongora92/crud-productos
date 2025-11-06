0236800ff2ec8a09d06e126a46363a4025bcffcf
Raul Gongora <raulgongora92@gmail.com>
2025-11-06 10:20:48 -0500

    Agregar ruta para listar productos

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const productos = require("./productos");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API de Productos funcionando 🚀");
+});
+
+app.get("/productos", (req, res) => {
+  res.json(productos);
 });
 
 app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
